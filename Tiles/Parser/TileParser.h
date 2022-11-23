21612e4835193146bdcf91573ee2b17d5afe2918
Dodos626 <chlktheo@gmail.com>
2022-11-23 16:00:39 +0200

    Tile parser ready
    a simple cpp parser

---
+++ Tiles/Parser/TileParser.h
@@ -0,0 +1,15 @@
+#ifndef TileParser
+#define TileParser
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+
+
+int TileParseFile(std::string filename);
+void PrintTileMap(std::vector<std::vector<int>> tileMap);
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "Tiles/Parser/TileParser.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //std::cout << "Hello, World!" << std::endl;
+    TileParseFile("UnitTest1Media/map1_Kachelebene 1.csv");
     return 0;
 }
