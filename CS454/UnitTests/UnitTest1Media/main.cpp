5835fd217ca68ff58d30a4b24c9a294b27781ce4
Dodos626 <chlktheo@gmail.com>
2022-11-25 12:02:06 +0200

    file restructure

---
+++ CS454/UnitTests/UnitTest1Media/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "TileParser.h"
+
+#define PATH "CS454/UnitTests/UnitTest1Media/"
+
+int main() {
+    //std::cout << "Hello, World!" << std::endl;
+    ReadTextMap("CS454/UnitTests/UnitTest1Media/media/map1_Kachelebene 1.csv");
+    return 0;
+}
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Tiles/Parser/TileParser.h"
-
-int main() {
-    //std::cout << "Hello, World!" << std::endl;
-    ReadTextMap("Media/UnitTest1Media/map1_Kachelebene 1.csv");
-    return 0;
-}
