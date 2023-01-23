801d716954355d4908736d29a7e4b6c56b81043d
Dodos626 <chlktheo@gmail.com>
2023-01-23 13:37:33 +0200

    Update Entity.h

--- CS454/Engine/Src/Entity/Entity.h
+++ CS454/Engine/Src/Entity/Entity.h
@@ -4,7 +4,7 @@
 #include "../Utils/Point.h"
 
 class Entity {
-private:
+protected:
 	Point coordinates; // where it is
 	int height, width; // size
 public:
@@ -19,9 +19,29 @@ public:
 	virtual void Render() = 0;
 	virtual bool Collides(Point p) = 0;
 	virtual void OnCollision() = 0; // if the player collides with it
+	virtual void Move() = 0; // creatures move items don't
+	virtual void Destroy() = 0; // in case of pickup / death
 	
 };
 
+class Item : Entity {
+	//just exists and is being rendered before enemies and player
+	//if it collides with a player it's picked up
+};
 
+class HealthPotion : Item {
+	//on pickup heals the player and then disappears
+};
+
+class Enemy : Entity {
+	//has a animator
+	//has a state
+	//has a health
+	//has a damage
+	//has a speed
+	//has points
+	//has on death chance to drop an item
+	//has an ai that takes players x,y and decides next move
+};
 
 #endif
