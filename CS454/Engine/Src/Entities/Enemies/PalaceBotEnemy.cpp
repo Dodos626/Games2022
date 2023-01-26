ae07bbd7fb678b1b159a355ef17f6dea19f35ce0
Dodos626 <chlktheo@gmail.com>
2023-01-26 14:13:15 +0200

    palace bot added

--- CS454/Engine/Src/Entities/Enemies/PalaceBotEnemy.cpp
+++ CS454/Engine/Src/Entities/Enemies/PalaceBotEnemy.cpp
@@ -2,8 +2,19 @@
 
 PalaceBotEnemy::PalaceBotEnemy(Point* spawn) : Enemy(spawn, "Engine/Configs/enemy/PalaceBotConfig.json") {
 	this->name = "PalaceBot";
+	
+	this->animator = new PlayerAnimator("Engine/Configs/enemy/PalaceBotAnimatorConfig.json", 0, { "palaceBot_red", "palaceBot_blue", "palaceBot_cyan" });
 }
 
 void PalaceBotEnemy::AI() {
 
+}
+
+void PalaceBotEnemy::Render(double curr_time) {
+	this->animator->render(this->coordinates->GetX(), this->coordinates->GetY(), curr_time, this->GetStateToInt(this->state));
+}
+
+
+int PalaceBotEnemy::GetStateToInt(palaceBot_state state) {
+	return static_cast<int>(state);
 }
--- CS454/Engine/Src/Entities/Enemies/PalaceBotEnemy.h
+++ CS454/Engine/Src/Entities/Enemies/PalaceBotEnemy.h
@@ -6,11 +6,20 @@
 #include "../../Animators/Animator.h"
 #include "../../Utils/Point.h"
 
+enum class palaceBot_state {
+	red,
+	blue,
+	cyan
+};
 
 class PalaceBotEnemy : public Enemy {
+private:
+	palaceBot_state state = palaceBot_state::red;
 public:
 	PalaceBotEnemy(Point* spawn);
 	void AI();
+	int GetStateToInt(palaceBot_state state);
+	void Render(double curr_time)override;
 };
 
 
