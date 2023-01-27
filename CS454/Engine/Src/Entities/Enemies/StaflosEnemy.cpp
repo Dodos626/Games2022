dad9a4e092f4cafb007676a3807b43389af868a8
Dodos626 <chlktheo@gmail.com>
2023-01-28 00:03:57 +0200

    staflos update

--- CS454/Engine/Src/Entities/Enemies/StaflosEnemy.cpp
+++ CS454/Engine/Src/Entities/Enemies/StaflosEnemy.cpp
@@ -10,7 +10,13 @@ StaflosEnemy::StaflosEnemy(Point* spawn, Action tryMoveLeft_, Action tryMoveRigh
 int StaflosEnemy::GetStateToInt(staflos_state state) {
 	return static_cast<int>(state);
 }
-
+/*
+enum class staflos_state {
+	move_left,
+	move_right,
+	atack_left,
+	atack_right
+};*/
 
 void StaflosEnemy::AI(Point player_position) {
 	//blepei ama trwei attack den kanei attack
@@ -19,11 +25,59 @@ void StaflosEnemy::AI(Point player_position) {
 	// ton kinigaei kai krataei ligi apostasi
 	// alliws meine akinitos
 	
+	if (this->takes_damage) { // an exei pathei damage
+		if (this->GetX() > player_position.GetX()) { // to attack irthe apo aristera
+			if (this->tryMoveUp(this->GetX(), this->GetY(), this->GetWidth(), this->GetHeight())) // kai mporei na sinexisei
+			{
+				this->MoveUp(); // three times create a small 
+				this->MoveUp(); // shaking effect
+				this->MoveUp();
+			}
+			if (this->tryMoveRight(this->GetX(), this->GetY(), this->GetWidth(), this->GetHeight())) // kai mporei na sinexisei
+				this->MoveRight();
 
+		}
+		else { // to attack irthe apo aristera
+			if (this->tryMoveUp(this->GetX(), this->GetY(), this->GetWidth(), this->GetHeight())) // kai mporei na sinexisei
+			{
+				this->MoveUp();
+				this->MoveUp();
+				this->MoveUp();
+			}
+			if (this->tryMoveLeft(this->GetX(), this->GetY(), this->GetWidth(), this->GetHeight())) // kai mporei na sinexisei
+				this->MoveLeft();
+
+		}
+		return;
+	}
+	//int x, int y, int width, int height
+	if (this->state == staflos_state::move_right) { // an paei deksia
+		if (this->tryMoveRight(this->GetX(), this->GetY(), this->GetWidth(), this->GetHeight()) && // kai mporei na sinexisei
+			!this->tryMoveDown(this->GetX() + 16, this->GetY(), this->GetWidth(), this->GetHeight())) { // xwris na pesei
+			this->MoveRight();
+		}
+		else {
+			this->state = staflos_state::move_left;
+		}
+	}
+	if (this->state == staflos_state::move_left) {
+		if (this->tryMoveLeft(this->GetX(), this->GetY(), this->GetWidth(), this->GetHeight()) && // kai mporei na sinexisei
+			!this->tryMoveDown(this->GetX() + 16, this->GetY(), this->GetWidth(), this->GetHeight())) { // xwris na pesei
+			this->MoveLeft();
+		}
+		else {
+			this->state = staflos_state::move_right;
+		}
+	}
 }
 
 void StaflosEnemy::Render(double curr_time, int relative_x) {
-	this->animator->render(this->coordinates->GetX() - relative_x, this->coordinates->GetY(), curr_time, this->GetStateToInt(this->state));
+	if (this->takes_damage) {
+		this->takes_damage = !this->animator->renderNframesOfAnimationWithFixFrame(this->coordinates->GetX() - relative_x, this->coordinates->GetY(), curr_time, 1);
+	}
+	else {
+		this->animator->render(this->coordinates->GetX() - relative_x, this->coordinates->GetY(), curr_time, this->GetStateToInt(this->state));
+	}
 }
 
 void StaflosEnemy::ChangeDirection() { // cannot change direction if attacking
