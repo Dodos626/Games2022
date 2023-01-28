5d56b196c1b5a9f55618f9759639f928cb0e01f6
Dodos626 <chlktheo@gmail.com>
2023-01-28 22:36:43 +0200

    music

--- CS454/Engine/Src/Misc/MusicPlayer/MusicPlayer.cpp
+++ CS454/Engine/Src/Misc/MusicPlayer/MusicPlayer.cpp
@@ -8,13 +8,18 @@ MusicPlayer::MusicPlayer() {
 	json data = json::parse(fin);
 	std::cout << data << std::endl;
 
-	std::string sample = data["songs_path"]["palace_theme"];
+	std::string sample = data["songs_path"]["main_theme"];
+	std::string sample2 = data["songs_path"]["palace_theme"];
+	
 	
 	this->samples.push_back(al_load_sample(sample.c_str()));
+	this->samples.push_back(al_load_sample(sample2.c_str()));
+	
 
 	this->volume = data["volume"];
 
 	this->index_playing = -1;
+	fin.close();
 
 }
 
@@ -23,7 +28,7 @@ void MusicPlayer::Play(int index) {
 			std::cerr << "Invalid index" << std::endl;
 			exit(EXIT_FAILURE);
 	}
-	
+	this->Stop();
 	this->index_playing = index;
 	al_play_sample(this->samples[index], this->volume, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
 	
